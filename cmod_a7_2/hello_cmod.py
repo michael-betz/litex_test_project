278022df6ce2e22b7ae56337142a101254754342
Michael Betz <mbetz@lbl.gov>
2019-03-08 17:48:28 -0800

    hello_cmod builds

--- cmod_a7_2/hello_cmod.py
+++ cmod_a7_2/hello_cmod.py
@@ -4,18 +4,20 @@ In FPGA world that is blinkng a LED
 """
 import sys
 from migen import *
-from cmod_a7 import *
-from base import _CRG
+from litex.boards.platforms.cmod_a7 import Platform
+from litex.boards.targets.cmod_a7 import _CRG
+
 
 class HelloCmod(_CRG):
     def __init__(self, platform):
-        _CRG.__init__(self, platform, sys_clk_freq=int(100e6))
+        _CRG.__init__(self, platform, int(100e6))
         led = platform.request("user_led")
         #  12e6 Hz / 2**(23) =  1.430511474609375 Hz
         # 100e6 Hz / 2**(23) = 11.920928955078125 Hz
         counter = Signal(23)
         self.comb += led.eq(counter[-1])
         self.sync += counter.eq(counter + 1)
+
 
 if __name__ == '__main__':
     p = Platform()
